cac1d9265b5f998d9ade444374f37c3ad7efc627
김민기 <kmg3833@naver.com>
2022-12-03 13:56:42 +0000

    practice, test departed

--- ProxyServer/practicefiles/hermes_json_practice.py
+++ ProxyServer/practicefiles/hermes_json_practice.py
@@ -25,34 +25,19 @@ def register():
         pass
 
 # 플라스크 세션 사용할 것; session.get
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         #return do_the_login()
-#         #test= request.form["test"]
-#         #test= request.json
-#         #test= request.get_json()
-#         #test= request.is_json()
-#         #test = request.host_url
-#         return f'{test.get_json()}'
-#     else:
-#         #return show_the_login_form()
-#         pass
-
-#플라스크 세션 사용할 것; session.get
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        user_id = request.form['id']
-        user_pw = request.form['password']
-        # form 데이터 검증 후
-        # 세션에 데이터 저장
-        session['id'] = user_id
-        return f'{test.get_json()}'
+        #return do_the_login()
+        test= request.form["test"]
+        #test= request.get_json()
+        #test= request.is_json()
+        #test = request.host_url
+        return f'{test}'
     else:
         #return show_the_login_form()
         pass
-    
+
 if __name__ == "__main__":
     #print(f"sqlAchemy version is {sqalc.__version__}")
     app.run(host='0.0.0.0', port=3833)
